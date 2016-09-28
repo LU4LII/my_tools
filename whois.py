7fffb793b6bd5e4be1d64ca66b6a80fc76ec81e1
Alessandro Pischedda <a.pischedda@nextworks.it>
2016-09-28 11:09:26 +0200

    resolved type check using isinstance. If wrong type exit from the script

--- whois.py
+++ whois.py
@@ -15,29 +15,34 @@ def decrypt(resp):
         text1 = resp[0:lugar-1]
         final2 = resp.find('</a>')
         text2 = resp[final2+4:]
-        resp = text1 + text2      
+        resp = text1 + text2
         lugar = resp.find("<a href")
     return(resp)
 
 domain =  input("dominio: ")
 domain = str.encode(domain)
 
-#P.O.G. simples pra ter certeza que tá gerando o tipo de dado correto
-print(type(domain))
+if not isinstance(domain, bytes):
+    print("Wrong type, expected 'bytes' instead of %s" %(type(domain)))
+    exit(-1)
 
 #valores para a request
 url = 'https://registro.br/cgi-bin/whois/'
 values = {}
 values['qr'] = domain
 
-#outro P.O.G. simples
-print(type(values))
+
+if not isinstance(values, dict):
+    print("Wrong type, expected 'dict' instead of %s" %(type(values)))
+    exit(-1);
 
 #request via POST
 data = urllib.parse.urlencode(values)
 
-#P.O.G. de novo
-print(type(data))
+if not isinstance(data, str):
+    print("Wrong type, expected 'str' instead of %s" %(type(data)))
+    exit(-1)
+
 data = str.encode(data)
 
 resp = urllib.request.urlopen(url, data).read()
